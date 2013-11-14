eee68af1106aab200204bc2abdc5aab8012eca8c
Sho Uemura <meeg@slac.stanford.edu>
2013-11-13 17:57:43 -0800

    add amplitude cut to time correlation

--- root/meeg_sourcetest.cpp
+++ root/meeg_sourcetest.cpp
@@ -644,6 +644,7 @@ int main ( int argc, char **argv ) {
 					clusterSum += hits[j];
 					clusterSize++;
 					hits[j] = 0.0;
+					if (max_E > 10.0*calSigma_mean[seed])
 					histT0_clustering->Fill(times[seed],times[j]);
 					j--;
 				}
@@ -652,6 +653,7 @@ int main ( int argc, char **argv ) {
 					clusterSum += hits[j];
 					clusterSize++;
 					hits[j] = 0.0;
+					if (max_E > 10.0*calSigma_mean[seed])
 					histT0_clustering->Fill(times[seed],times[j]);
 					j++;
 				}
